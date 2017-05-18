5d5e752c0faec41e88abd08f8b8a20afc6f434f3
James Harrison <jharrison23@knights.ucf.edu>
2017-05-18 13:50:24 -0400

    Linked to an api.ai bot and returns the json which api.ai sends

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+var accessToken = "5489544adf6d490c8438cb7377f4bd60";
+var baseUrl = "https://api.api.ai/v1/";
+
+
 var $chatlogs = $('.chatlogs');
 
-$("textarea").keypress(function(event){
+$("textarea").keypress(function(event) {
     
-    if(event.which === 13){
+    if(event.which === 13) {
+        event.preventDefault();
         console.log("enter");
-
+	    send(this.value);
         newSentMessage(this.value);
         this.value = "";
 
@@ -32,4 +37,30 @@ function newRecievedMessage(messageText){
 
 
 }
+
+
+
+function send(text) {
+	$.ajax({
+		type: "POST",
+		url: baseUrl + "query?v=20150910",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		headers: {
+			"Authorization": "Bearer " + accessToken
+		},
+		data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
+		success: function(data) {
+			setResponse(JSON.stringify(data, undefined, 2));
+		},
+		error: function() {
+			setResponse("Internal Server Error");
+		}
+	});
+	setResponse("Loading...");
+}
+
+function setResponse(val) {
+			newRecievedMessage(val);
+		}
 
