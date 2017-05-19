86d0e74b4f02cfce4d13d8bf428012858f9c284f
James Harrison <jharrison23@knights.ucf.edu>
2017-05-19 14:41:41 -0400

    Hid the text area and send button whenever the user sends a message

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ $("textarea").keypress(function(event) {
     }
 });
 
+//$(".sendButton").on("click", newSentMessage($("textarea").value));
+
 function newSentMessage(messageText){
     // $chatlogs.append(
     //     $('<div/>', {'class': 'chat self'}).append(
@@ -23,15 +25,43 @@ function newSentMessage(messageText){
 
 	$chatlogs.append(
         $('<div/>', {'class': 'chat self'}).append(
-            $('<p/>', {'class': 'chat-message', 'text': messageText})));		
+            $('<p/>', {'class': 'chat-message', 'text': messageText})));
+
+	
+	$('.sendButton').css('visibility', 'hidden');
+	$('textarea').css('visibility', 'hidden');
+	setResponse("...");
+
+
+	// $('.sendButton').html('Wait');
+	// $('.sendButton').css("background", "gray");
+	// $('.sendButton').css("box-shadow", "gray");
+
+
+	// setTimeout(function(){
+	// 	console.log("in here");
+	// 	// $('.sendButton').html('Send');
+	// 	$('.sendButton').css('visibility', 'visible');
+	// 	$('textarea').css('visibility', 'visible');
+
+
+	// 	// $('.sendButton').css("background", "orange");
+	// 	// $('.sendButton').css("box-shadow", "orange");
+	// 	// $('.sendButton').prop('disabled', false);
+	// }, 3000);		
 }
 
 function newRecievedMessage(messageText){
     $chatlogs.append(
         $('<div/>', {'class': 'chat friend'}).append(
-            $('<div/>', {'class': 'user-photo'}).append($('<img src="ana.png" />')), 
+            $('<div/>', {'class': 'user-photo'}).append($('<img src="ana.JPG" />')), 
             $('<p/>', {'class': 'chat-message', 'text': messageText})));
-    $chatlogs.scrollBottom;
+
+	// setTimeout(function(){
+	// 	$('.input').prop('disabled', false);
+	// }, 5000);
+	// $('.input').prop('disabled', false);
+
 
 
 }
@@ -48,17 +78,33 @@ function send(text) {
 		data: JSON.stringify({ query: text, lang: "en", sessionId: "somerandomthing" }),
 		success: function(data) {
             console.log(data);
+			
+		setTimeout(function(){
+		
+			$('.sendButton').css('visibility', 'visible');
+			$('textarea').css('visibility', 'visible');
+
 			setResponse(JSON.stringify(data.result.fulfillment.speech, undefined, 2));
+
+		// $('.sendButton').css("background", "orange");
+		// $('.sendButton').css("box-shadow", "orange");
+		// $('.sendButton').prop('disabled', false);
+		}, 3000);
+
+		
+			
 		},
 		error: function() {
 			setResponse("Internal Server Error");
 		}
 	});
-	setResponse("...");
+	// setResponse("...");
 }
 
 
 function setResponse(json) {
     newRecievedMessage(json);
 }
+
+
 
