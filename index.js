239731f52e5e0415252b69ff851f6ae734028146
James Harrison <jharrison23@knights.ucf.edu>
2017-07-17 01:28:40 -0400

    Updated the UI and changed some images

--- index.js
+++ index.js
@@ -558,3 +558,22 @@ function speechResponse(message)
   	window.speechSynthesis.speak(msg);
 
 }
+
+
+
+
+
+$(document)
+    .one('focus.input', 'textarea.input', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.input', 'textarea.input', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows;
+	});
+	
